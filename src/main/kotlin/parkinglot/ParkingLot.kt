745b3f6aad89e7cbe9e2f3f7f4fe6468ab51817a
suchitra <suchitra9049@gmail.com>
2023-09-05 16:17:28 +0530

    Refactor. extract to named methods

--- src/main/kotlin/parkinglot/ParkingLot.kt
+++ src/main/kotlin/parkinglot/ParkingLot.kt
@@ -1,16 +1,21 @@
 package parkinglot
 
+
 class ParkingLot(private val capacity: Int) {
     private val slots: MutableList<Any> = ArrayList()
 
     fun park(vehicle: Any) {
-        if (slots.contains(vehicle)) {
+        if (isParked(vehicle)) {
             throw AlreadyParkedException()
         }
-        if (slots.size == capacity) {
+        if (isFull()) {
             throw LotFullException()
         }
         slots.add(vehicle)
     }
+
+    private fun isFull(): Boolean = slots.size == capacity
+
+    private fun isParked(vehicle: Any): Boolean = slots.contains(vehicle)
 
 }
