3909030f149466367ebab978dd3cd2dce11f73dd
suchitra <suchitra9049@gmail.com>
2023-12-04 20:01:53 +0530

    Add. throw exception for parking already parked vehicle

--- src/main/kotlin/parkinglot/ParkingLot.kt
+++ src/main/kotlin/parkinglot/ParkingLot.kt
@@ -4,6 +4,9 @@ class ParkingLot(private val capacity: Int) {
     private val vehicles: ArrayList<Any> = ArrayList()
 
     fun park(vehicle: Any) {
+        if (vehicles.contains(vehicle)) {
+            throw AlreadyParkedException("vehicle already parked")
+        }
         if (vehicles.size == capacity) {
             throw LotFullException("lot is full")
         }
--- src/test/kotlin/parkinglot/ParkingLotTest.kt
+++ src/test/kotlin/parkinglot/ParkingLotTest.kt
@@ -22,10 +22,21 @@ class ParkingLotTest {
 
     @Test
     internal fun `should not park car when lot is full`() {
-        val parkingLot = ParkingLot(0)
+        val parkingLot = ParkingLot(1)
         val car = car()
+        val anotherCar = car()
+        parkingLot.park(car)
 
-        invoking { parkingLot.park(car) } shouldThrow LotFullException::class withMessage "lot is full"
+        invoking { parkingLot.park(anotherCar) } shouldThrow LotFullException::class withMessage "lot is full"
+    }
+
+    @Test
+    internal fun `should not park car when already parked`() {
+        val parkingLot = ParkingLot(2)
+        val car = car()
+        parkingLot.park(car)
+
+        invoking { parkingLot.park(car) } shouldThrow AlreadyParkedException::class withMessage "vehicle already parked"
     }
 
     private fun car(): Any {
