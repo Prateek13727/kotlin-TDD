0a5fd68b21efdd1f32852c554cf03aa1fc21e837
Monika <monika@Coffeebeanss-MacBook-Pro.local>
2023-12-13 18:57:14 +0530

    Refactor. Remove redundant code

--- src/main/kotlin/parkinglotv2/domain/usecases/external/AttendantParkVehicle.kt
+++ src/main/kotlin/parkinglotv2/domain/usecases/external/AttendantParkVehicle.kt
@@ -1,10 +1,7 @@
 package parkinglotv2.domain.usecases.external
 
-import parkinglotv2.domain.entities.Attendant
-import parkinglotv2.domain.entities.ParkingLot
 import parkinglotv2.domain.repos.AttendantsRepo
 import parkinglotv2.domain.repos.VehiclesRepo
-import parkinglotv2.domain.usecases.LotFullException
 import parkinglotv2.domain.usecases.internal.ParkVehicleInParkingLot
 
 class AttendantParkVehicle(
@@ -17,16 +14,9 @@ class AttendantParkVehicle(
     val attendant = attendantsRepo.get(attendantId) ?: return
     val vehicle = vehiclesRepo.getVehicle(vehicleId) ?: return
 
-    val availableLots = getAvailableParkingLots(attendant)
-//    if (availableLots.isEmpty()) throw LotFullException()
+    val availableLots = attendant.parkingLots
 
     parkVehicleInParkingLot.invoke(availableLots.first(), vehicle)
   }
 
-  private suspend fun getAvailableParkingLots(attendant: Attendant): List<ParkingLot> =
-    attendant.parkingLots
-      .filter { hasEmptySlot(it) }
-
-  private fun hasEmptySlot(it: ParkingLot) =
-    it.slots.count { it.vehicle == null } > 0
 }
